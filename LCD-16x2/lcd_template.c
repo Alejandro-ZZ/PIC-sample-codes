2c94ff64c3354feb04aff982ac99653dbc5b11f4
Alejandro-ZZ <71833624+Alejandro-ZZ@users.noreply.github.com>
2020-10-03 13:15:53 -0500

    Update lcd_template.c

--- LCD-16x2/lcd_template.c
+++ LCD-16x2/lcd_template.c
@@ -8,10 +8,10 @@
 #define		lcd_rs_pin		  pin_D0
 #define		lcd_rw_pin		  pin_D1
 #define		lcd_enable_pin	pin_D2
-#define		lcd_data4		    pin_D4
-#define		lcd_data5		    pin_D5
-#define		lcd_data6		    pin_D6
-#define		lcd_data7		    pin_D7
+#define		lcd_data4		   pin_D4
+#define		lcd_data5		   pin_D5
+#define		lcd_data6		   pin_D6
+#define		lcd_data7		   pin_D7
 
 #include <lcd.c>
 
@@ -20,13 +20,13 @@ float x=3.5;
 void main() {
   
   //Setup code
-  lcd_init(); 				//inicializa el LCD
+  lcd_init(); 	//LCD display initialization
 	
-  //Main code that runs repeatedly
+  	//Main code that runs repeatedly
 	while(TRUE) {
     
-    //Shows at position (0,0) 'Number: 3.5'
-    //%0.1f means that x is a float type variable and shows just 1 decimal
+    		//Shows at position (0,0) 'Number: 3.5'
+    		//%0.1f means that x is a float type variable and shows just 1 decimal
 		printf(lcd_putc,"\fNumber: %0.1f",x);	//Enviar datos-[\f vaya a la posicion (0,0)]
 		delay_ms(350);
     
